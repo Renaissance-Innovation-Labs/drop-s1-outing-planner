2fef07633cf66a310e152e02ebc2d92548c03fd2
droxy <chiomaotu22@gmail.com>
2024-06-28 06:47:16 +0100

    added icons

--- public/icons/index.ts
+++ public/icons/index.ts
@@ -1,4 +1,7 @@
 import caretLeft from './caret-left.svg';
 import caretRight from './caret-right.svg';
+import outynLogo from './logo.svg';
+import outynLogoLarge from './outyn-logo.svg';
+import outynGlass from './outyn-glass.svg';
 
-export { caretLeft, caretRight };
+export { caretLeft, caretRight, outynLogo, outynLogoLarge, outynGlass };
